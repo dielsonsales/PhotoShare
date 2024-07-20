1a4a42bb8d0a4e726d3deaf1379c41de5e3d701a
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2024-07-20 14:39:50 -0300

    Initial Commit

---
+++ PhotoShare/PhotoShareApp.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoShareApp.swift
+//  PhotoShare
+//
+//  Created by Dielson Sales de Carvalho on 20/07/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PhotoShareApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
