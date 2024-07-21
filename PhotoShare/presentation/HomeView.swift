41f925ecef7e4dad0412f85edf2fbec7ab56a6ef
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2024-07-21 18:29:42 -0300

    Move post item to a separate view

--- PhotoShare/presentation/HomeView.swift
+++ PhotoShare/presentation/HomeView.swift
@@ -20,47 +20,56 @@ struct HomeView: View {
     var body: some View {
         VStack {
             ScrollView {
-                VStack {
-                    HStack {
-                        Rectangle()
-                            .fill(Color.blue)
-                            .background(Color.blue)
-                            .frame(width: 30, height: 30)
-                            .cornerRadius(10)
-                            .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 0))
-                        VStack(alignment: .leading) {
-                            Text("retrocomputers")
-                                .font(.caption)
-                            Text("retrocomputers_")
-                                .font(.caption)
-                        }
-                        Spacer()
-                        Image(systemName: "ellipsis")
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.blue)
-                    }
-                    .frame(minWidth: nil, maxWidth: .infinity, minHeight: 400, maxHeight: 400)
-                    HStack {
-                        Image(systemName: "heart")
-                            .imageScale(.large)
-                        Image(systemName: "message")
-                            .imageScale(.large)
-                        Image(systemName: "paperplane")
-                            .imageScale(.large)
-                        Spacer()
-                    }
-                    .padding(EdgeInsets(top: 1, leading: 8, bottom: 0, trailing: 8))
-                }
-                .background(Color.red)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
+                PostItem()
+                PostItem()
+                PostItem()
             }
-            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
         }
+    }
+}
+
+struct PostItem: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Rectangle()
+                    .fill(Color.blue)
+                    .background(Color.blue)
+                    .frame(width: 30, height: 30)
+                    .cornerRadius(10)
+                    .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 0))
+                VStack(alignment: .leading) {
+                    Text("retrocomputers")
+                        .font(.caption)
+                    Text("retrocomputers_")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "ellipsis")
+            }
+            .frame(maxWidth: .infinity)
+            .padding(EdgeInsets(top: 8, leading: 8, bottom: 0, trailing: 8))
+            ZStack {
+                Rectangle()
+                    .fill(Color.blue)
+            }
+            .frame(minWidth: nil, maxWidth: .infinity, minHeight: 400, maxHeight: 400)
+            HStack {
+                Image(systemName: "heart")
+                    .imageScale(.large)
+                Image(systemName: "message")
+                    .imageScale(.large)
+                Image(systemName: "paperplane")
+                    .imageScale(.large)
+                Spacer()
+                Image(systemName: "star")
+                    .imageScale(.large)
+            }
+            .padding(EdgeInsets(top: 1, leading: 8, bottom: 8, trailing: 8))
+        }
+        .background(Color.red)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
     }
 }
 
