3e30d5b39593f57df5ed614708883d063b8996c9
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2024-07-28 19:53:27 -0300

    Create PagerView for the profile

---
+++ PhotoShare/presentation/PagerView.swift
@@ -0,0 +1,64 @@
+//  PhotoShare
+//  Copyright (C) 2024  Dielson Sales de Carvalho.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see https://www.gnu.org/licenses/.
+
+import SwiftUI
+
+struct PagerView: View {
+    @State private var selectedIndex = 0
+    let icons: [String]
+
+    var body: some View {
+        VStack {
+            // Tab bar
+            HStack {
+                ForEach(0..<icons.count, id: \.self) { index in
+                    Button(action: {
+                        selectedIndex = index
+                    }, label: {
+                        Image(systemName: icons[index])
+                            .imageScale(.large)
+                            .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                        if index < icons.count - 1 {
+                            Spacer()
+                        }
+                    })
+                    .foregroundStyle(selectedIndex == index ? Color.black : Color.gray)
+                }
+            }
+            // Content view
+            TabView(selection: $selectedIndex) {
+                ForEach(0..<icons.count, id: \.self) { index in
+                    Rectangle()
+                        .fill(index == 0 ? Color.blue: Color.yellow)
+                        .tag(index)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        let icons: [String] = [
+            "squareshape.split.3x3",
+            "video.bubble",
+            "tag"
+        ]
+        PagerView(icons: icons)
+            .modelContainer(for: Item.self, inMemory: true)
+    }
+}
--- PhotoShare/presentation/Profile/ProfileView.swift
+++ PhotoShare/presentation/Profile/ProfileView.swift
@@ -18,80 +18,101 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                topBar
-                HStack(alignment: .center, spacing: 16) {
-                    VStack(alignment: .center, spacing: 8) {
-                        Rectangle()
-                            .fill(Color.blue)
-                            .frame(width: 60, height: 60)
-                            .cornerRadius(30)
-                        Text("Dielson Sales")
+        GeometryReader { geometry in
+            let gridSize = (geometry.size.width - 4 * 8) / 3
+            ScrollView {
+                VStack {
+                    topBar
+                    HStack(alignment: .center, spacing: 16) {
+                        VStack(alignment: .center, spacing: 8) {
+                            Rectangle()
+                                .fill(Color.blue)
+                                .frame(width: 60, height: 60)
+                                .cornerRadius(30)
+                            Text("Dielson Sales")
+                                .font(.callout)
+                        }
+                        Spacer()
+                        VStack(alignment: .center, spacing: 8) {
+                            Text("21")
+                            Text("posts")
+                                .font(.subheadline)
+                                .lineLimit(1)
+                        }
+                        Spacer()
+                        VStack(alignment: .center, spacing: 8) {
+                            Text("75")
+                            Text("followers")
+                                .font(.subheadline)
+                                .lineLimit(1)
+                        }
+                        Spacer()
+                        VStack(alignment: .center, spacing: 8) {
+                            Text("146")
+                            Text("following")
+                                .font(.subheadline)
+                                .lineLimit(1)
+                        }
+                    }
+                    .frame(height: 110, alignment: .center)
+                    HStack(alignment: .center) {
+                        Text("Learning to survive anxiety")
                             .font(.callout)
+                        Spacer()
                     }
-                    Spacer()
-                    VStack(alignment: .center, spacing: 8) {
-                        Text("21")
-                        Text("posts")
-                            .font(.subheadline)
-                            .lineLimit(1)
+                    .frame(height: 40)
+                    profileButtons
+                    HStack {
+                        let padding = gridSize / 2
+                        Button(action: {
+                            // TODO:
+                        }, label: {
+                            Image(systemName: "squareshape.split.3x3")
+                                .imageScale(.large)
+                        })
+                        .frame(maxHeight: .infinity)
+                        .foregroundStyle(.black)
+                        .padding(EdgeInsets(top: 8, leading: padding, bottom: 8, trailing: 0))
+                        Spacer()
+                        Button(action: {
+                            // TODO:
+                        }, label: {
+                            Image(systemName: "video.bubble")
+                                .imageScale(.large)
+                        })
+                        .frame(maxHeight: .infinity)
+                        .foregroundStyle(.black)
+                        .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                        Spacer()
+                        Button(action: {
+                            // TODO:
+                        }, label: {
+                            Image(systemName: "tag")
+                                .imageScale(.large)
+                        })
+                        .frame(maxHeight: .infinity)
+                        .foregroundStyle(.black)
+                        .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: padding))
                     }
-                    Spacer()
-                    VStack(alignment: .center, spacing: 8) {
-                        Text("75")
-                        Text("followers")
-                            .font(.subheadline)
-                            .lineLimit(1)
+                    .frame(height: 40, alignment: .center)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .top, spacing: 0) {
+                            Rectangle()
+                                .fill(Color.blue)
+                                .frame(width: geometry.size.width - 32)
+                            Rectangle()
+                                .fill(Color.red)
+                                .frame(width: geometry.size.width - 32)
+                            Rectangle()
+                                .fill(Color.brown)
+                                .frame(width: geometry.size.width - 32)
+                        }
+                        .frame(height: 1000)
                     }
-                    Spacer()
-                    VStack(alignment: .center, spacing: 8) {
-                        Text("146")
-                        Text("following")
-                            .font(.subheadline)
-                            .lineLimit(1)
-                    }
-                }
-                HStack(alignment: .center) {
-                    Text("Learning to survive anxiety")
-                        .font(.callout)
-                    Spacer()
-                }
-                profileButtons
-                HStack {
-                    Button(action: {
-                        // TODO:
-                    }, label: {
-                        Image(systemName: "squareshape.split.3x3")
-                            .imageScale(.large)
-                    })
-                    .frame(maxHeight: .infinity)
-                    .foregroundStyle(.black)
-                    .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
-                    Spacer()
-                    Button(action: {
-                        // TODO:
-                    }, label: {
-                        Image(systemName: "video.bubble")
-                            .imageScale(.large)
-                    })
-                    .frame(maxHeight: .infinity)
-                    .foregroundStyle(.black)
-                    .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
-                    Spacer()
-                    Button(action: {
-                        // TODO:
-                    }, label: {
-                        Image(systemName: "tag")
-                            .imageScale(.large)
-                    })
-                    .frame(maxHeight: .infinity)
-                    .foregroundStyle(.black)
-                    .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
                 }
             }
+            .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
         }
-        .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
     }
 
     var topBar: some View {
@@ -115,6 +136,7 @@ struct ProfileView: View {
             })
             .foregroundStyle(.black)
         }
+        .frame(height: 40, alignment: .center)
     }
 
     var profileButtons: some View {
